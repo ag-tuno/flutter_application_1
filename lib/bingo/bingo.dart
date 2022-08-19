6d76092db282ad6eabb7aa9763bcc29c6e1224d6
t.uno <joyportfield@gmail.com>
2022-08-19 18:03:05 +0900

    update:bingo_bloc

--- lib/bingo/bingo.dart
+++ lib/bingo/bingo.dart
@@ -1,34 +1,75 @@
-
 import 'package:flutter/material.dart';
-import './bingo_bloc.dart';
+import 'package:flutter_application_1/bingo/bingo_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
 
 class BingoPage extends StatelessWidget {
   const BingoPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Bingo Page'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/bingo');
-                },
-              child: const Text('hoge'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/config');
-                },
-              child: const Text('fuga'),
-            ),
-          ],
+    return MultiProvider(
+      providers: [
+        BlocProvider<BingoBloc>(
+          create: (BuildContext context) => BingoBloc(),
         ),
+      ],
+      child: _Contents(),
+    );
+  }
+}
+
+class _Contents extends StatelessWidget {
+  List<Panel> panels = [];
+  _Contents() {
+    for(int i = 0; i < 25; i++) {
+      panels.add(Panel());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Bingo Page')),
+      body: BlocBuilder<BingoBloc, Panel>(
+        builder: (context, state) {
+          return GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 5, //カラム数
+            ),
+            itemCount: 25, //要素数
+            itemBuilder: (context, index) {
+              //要素を戻り値で返す
+              return GestureDetector(
+                child: GridTile(
+                  child: Container(
+                    color: index.isEven ? Colors.blue : Colors.yellow,
+                  )
+                ),
+                onTap: () {
+                  context.read<BingoBloc>().add(TapPanel());
+                },
+              );
+            },
+            shrinkWrap: true,
+          );
+
+          // return OutlinedButton(
+          //   onPressed: () {
+          //     debugPrint('Received click');
+          //   },
+          //   child: const Text('100'),
+          // );
+
+          // return GestureDetector(
+          //   child: Container(
+          //     child: const Text('100'),
+          //   ),
+          //   onTap: () {
+          //     debugPrint('Received click');
+          //   }
+          // );
+        }
       ),
     );
   }
--- lib/bingo/bingo_bloc.dart
+++ lib/bingo/bingo_bloc.dart
@@ -3,12 +3,31 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 abstract class BingoEvent {}
 
 class TapPanel extends BingoEvent {}
- 
 
-class BingoBloc extends Bloc<BingoEvent, bool> {
-  BingoBloc() : super(false) {
-    on<TapPanel>((event, emit) => emit(state));
+class BingoBloc extends Bloc<BingoEvent, Panel> {
+  BingoBloc() : super(Panel()) {
+    on<TapPanel>((event, emit) => emit(_tapPanel(state, event)));
   }
+}
+
+Panel _tapPanel(panel, event) {
+  panel.isTapped = true;
+  panel.state = PanelState.open;
+  
+  switch(panel.state) {
+    case PanelState.close:
+      break;
+    case PanelState.open:
+      break;
+    case PanelState.reach:
+      break;
+    case PanelState.clear:
+      break;
+    default:
+      print('unknown state');
+  }
+
+  return panel;
 }
 
 class Panel {
@@ -17,7 +36,7 @@ class Panel {
   PanelState state = PanelState.close;
 }
 
-// パネルの状態
+// パネルの表示状態
 enum PanelState {
   close,
   open,
